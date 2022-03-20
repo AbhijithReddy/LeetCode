6e4495181ae12cd85bd89ef6421cdce7461a7bfd
Abhijith Reddy <abhijithreddy.a@gmail.com>
2022-03-20 00:57:08 -0400

    Time: 562 ms (33.07%), Space: 0B (100.00%) - LeetHub

---
+++ 180-consecutive-numbers/180-consecutive-numbers.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT distinct(innr.num) as ConsecutiveNums FROM (
+SELECT num,
+LEAD(num) OVER w as 'LEAD',
+LAG(num) OVER w as 'LAG'
+FROM Logs
+WINDOW w AS (ORDER BY id)
+) as innr
+WHERE innr.LEAD = innr.num AND innr.LAG = innr.num
