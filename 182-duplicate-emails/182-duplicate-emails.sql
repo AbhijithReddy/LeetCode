3742b561d9913eb5ab859c0080a7f9d88e17993a
Abhijith Reddy <abhijithreddy.a@gmail.com>
2022-03-24 02:14:29 -0400

    Time: 379 ms (44.70%), Space: 0B (100.00%) - LeetHub

--- 182-duplicate-emails/182-duplicate-emails.sql
+++ 182-duplicate-emails/182-duplicate-emails.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
-SELECT email
-FROM Person
-GROUP BY email
-HAVING COUNT(email) > 1
+SELECT distinct p1.email
+FROM Person p1
+JOIN Person p2
+ON p1.email = p2.email
+AND p1.id <> p2.id
